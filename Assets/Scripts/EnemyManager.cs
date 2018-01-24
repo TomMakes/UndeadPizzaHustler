df1ae6f013b8df4f34a79fd4124c9ae2be6b75ee
MAIN\jrp8297 <jrp8297@g.rit.edu>
2018-01-24 11:05:22 -0500

    Addition of commented psuedocode

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -16,9 +16,28 @@ public class EnemyManager : MonoBehaviour {
 	void Update () {
         foreach(GameObject obby in zedList)
         {
+            ZedScript currentScript = obby.GetComponent<ZedScript>();
+            //Check if its the farthest out
 
+        //check if its far enough to be colliding
 
+        //check if its in the right lane
 
+        //handle collisions
+
+        }
+
+        //Spawn conditional Check
+        {
+            //Check the previous safe lanes
+            //randomize new dangerous lanes
+            //ensure that the new safe lanes are within 1 lane of the old safe lanes?
+            //bitwise math
+            // 0  1  2  3  4  5  6  7  8  9
+            // Z O O O  Z  Z  Z  O  O  O
+            // O Z O O  Z  O  O  Z  Z  O
+            // O O Z O  O  Z  O  Z  O  Z
+            // O O O Z  O  O  Z  O  Z  Z
 
         }
 		
