25e2937520be2d7076301d9dbac61587d1ebfb68
Thomas Ryszkiewicz <tcr4595@rit.edu>
2018-01-25 20:56:43 -0500

    Made the EnemyManager script and ZedScript, the ZedScript currently does not work however, and will crash unity when it is ran.
    EnemyManager: Handles placing the enemies on the screen (maybe in future have it deal with collisions?)
    
    ZedScript: Moves the enemies from the right of the screen to the left (start point to end point) depending on at what speed it is set.
                       Bool values say which lanes will hurt the player and which are safe.
                       May handle collision checking in the future.

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -15,12 +15,24 @@ public class EnemyManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         foreach(GameObject obby in zedList)
-        {
+		{
+			//Grab the script from the enemy
+			ZedScript zedScript = obby.GetComponent<ZedScript> ();
 
-
-
-
+			//If there is an enemy currently running through the game
+			if (zedScript.isActive == true) {
+				break;
+			}
+				
+			//If there are more than one enemy type take a random enemy obstacle and run it back through the screen
+			if (zedList.Count > 1) {
+				zedList [Random.Range (0, zedList.Count-1)].GetComponent<ZedScript> ().Reset ();
+			}
+			else{
+				zedList[0].GetComponent<ZedScript> ().Reset ();
         }
+
+
 		
 	}
 }
--- Assets/Scripts/ZedScript.cs
+++ Assets/Scripts/ZedScript.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class ZedScript : MonoBehaviour {
 
+	//Speed at which enemy travels from right to left of screen
+	public Vector3 speed;
+
+	//Is this lane a safe or deadly lane? True means it is a safe lane
+	public bool lane0, lane1, lane2, lane3;
+
+
+	//Where to spawn/place the enemy and where to remove it from the game.
+	public GameObject startPoint;
+	public GameObject endPoint;
+
+	public bool isActive = false;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		//Make the enemy begin at the start point
+		gameObject.transform.position = startPoint.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Keep moving the enemy to the left of the screen
+		while (gameObject.transform.position.x > endPoint.transform.position.x) {
+			gameObject.transform.position -= speed;
+			isActive = true;
+		}
+		isActive = false;
+
+	}
+
+	public void Reset()
+	{
+		gameObject.transform.position = startPoint.transform.position;
+	}
+
+	bool IsColliding()
+	{
 		
 	}
 }
