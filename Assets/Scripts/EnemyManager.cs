99c433a7956e20270c8fca0a5d21c2604bee1759
Morrgan Sweeney-Charlton <mxs4952@rit.edu>
2018-01-29 13:03:57 -0500

    Created this branch. Parallax sprite is now attached to a prefab. Zombies now delete on contact with player object or on leaving left side of screen. Parallax now moves using Time.deltaTime instead of a static number, and has a public speed variable. Minor optimization to zombie collision detection code.

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -2,32 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyManager : MonoBehaviour {
+public class EnemyManager : MonoBehaviour
+{
     public List<GameObject> zedList;
-	public bool runEnemy, recentlySpawned=false, toDelete=false;
-    public float spawnBoundry=8.0f;//point at which to spawn a new zed
+    public bool runEnemy, recentlySpawned = false, toDelete = false;
+    public float spawnBoundry = 8.0f;//point at which to spawn a new zed
     public int maxZeds = 4;
     public GameObject ZedPrefab;
     public GameObject Player;
     PlayerManager playerScript;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         playerScript = Player.GetComponent<PlayerManager>();
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-		foreach (GameObject obby in zedList) {
-			//Grab the script from the enemy
-			ZedScript zedScript = obby.GetComponent<ZedScript> ();
-			
-		}
+    }
 
-		if (runEnemy) {
-            if (zedList.Count < maxZeds && ! recentlySpawned)
+    // Update is called once per frame
+    void Update()
+    {
+
+        foreach (GameObject obby in zedList)
+        {
+            //Grab the script from the enemy
+            ZedScript zedScript = obby.GetComponent<ZedScript>();
+
+        }
+
+        if (runEnemy)
+        {
+            if (zedList.Count < maxZeds && !recentlySpawned)
             {
                 zedList.Add(Instantiate(ZedPrefab));
                 ZedScript temp = zedList[zedList.Count - 1].GetComponent<ZedScript>();
@@ -37,22 +42,25 @@ public class EnemyManager : MonoBehaviour {
 
             int i = 0;//only need to check the 3 closest zeds for collisions.
             recentlySpawned = false;
-            foreach (GameObject obby in zedList) {
-				//Grab the script from the enemy
-				ZedScript zeddy = obby.GetComponent<ZedScript> ();          
-                if(zeddy.isActive)
+            foreach (GameObject obby in zedList)
+            {
+                //Grab the script from the enemy
+                ZedScript zeddy = obby.GetComponent<ZedScript>();
+                if (zeddy.isActive)
                 {
-                 if(zeddy.transform.position.x > spawnBoundry)
+                    if (zeddy.transform.position.x > spawnBoundry)
                     {
                         recentlySpawned = true;
                     }
 
-                 if(i<=4)
+                    if (i <= 4)
                     {
-                        if(zeddy.position.x < Player.transform.position.x + 1.25f)//CHANGE THIS TO PLAYER WIDTH VARIBLE LATER
+                        if (zeddy.whichLane == playerScript.layer)
                         {//if the first 3 zeds are close enough to the player to maybe hit, check em
-                            if(zeddy.whichLane == playerScript.layer){
+                            if (zeddy.position.x < Player.transform.position.x + 1.25f && zeddy.position.x > Player.transform.position.x - 1.25f)//CHANGE THIS TO PLAYER WIDTH VARIBLE LATER
+                            {
                                 Debug.Log("HIT!");
+                                toDelete = true;
                             }
                         }
                         i++;
@@ -67,18 +75,20 @@ public class EnemyManager : MonoBehaviour {
                 }
 
 
-                if(toDelete==true)
+                if (toDelete == true)
                 {//garbage collection
-                    zedList.RemoveAt(0);
+                    Object.Destroy(obby);
+                    zedList.Remove(obby);
+                    //zedList.RemoveAt(0);
                     toDelete = false;
                 }
-				
-				//If there are more than one enemy type take a random enemy obstacle and run it back through the screen
-			
-			}
-		}
+
+                //If there are more than one enemy type take a random enemy obstacle and run it back through the screen
+
+            }
+        }
 
 
-		
-	}
+
+    }
 }
