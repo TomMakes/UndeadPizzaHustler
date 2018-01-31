2a24ec5cd4514cce052c1c909d59e3701ad81b68
seank0h <seankoh754@gmail.com>
2018-01-30 22:51:14 -0500

    Defeat Screen and GameUI

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour {
     public int layer=0;
     Vector3 newPosition;
+    public int lives;
+    public int score = 0;
+    float time=0;
+    bool ifExist;
+    public Text scoreText;
+    public Image lifebar;
+    public Sprite health2;
+    public Sprite health1;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start ()
+    {
 
         newPosition = gameObject.transform.position;
+        PlayerPrefs.SetInt("GameScore", score);
+        
+        ifExist = true;
+        
 	}
 	
 	// Update is called once per frame
@@ -39,7 +49,28 @@ public class PlayerManager : MonoBehaviour {
             }
 
         }
-
+        time += Time.deltaTime;
+        score = (int)time * 100;
+        scoreText.text = "Score: " + score;
         gameObject.transform.position = newPosition;
+        if (lives == 2)
+        {
+            lifebar.sprite = health2;
+        }
+        if (lives == 1)
+        {
+            lifebar.sprite = health1;
+        }
+        if (lives <= 0)
+        {
+            PlayerPrefs.SetInt("GameScore", score);
+            SceneManager.LoadScene("GameOverScene");
+
+        }
+    }
+    public void OnHit(int hit,int penalty)
+    {
+        lives -= hit;
+        score -= penalty;
     }
 }
